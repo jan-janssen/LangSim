67a56526521eac6a8e3d14ed8e0024a2d618e995
Utkarshp1161 <utkarshp1161@gmail.com>
2024-05-08 10:15:55 -0400

    fix: path

--- example_llama3_hugging_face/react_agent.py
+++ example_llama3_hugging_face/react_agent.py
@@ -14,6 +14,7 @@ logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
 from llama_index.core.tools import QueryEngineTool, ToolMetadata
 import argparse
+import os
 
 def parse_args():
   parser = argparse.ArgumentParser()
@@ -22,86 +23,89 @@ def parse_args():
   return parser.parse_args()
 
 
-args = parse_args()
-data_dir = args.data_dir
-api_key = args.groq_api_key
-# load data
-ASE_doc1 = SimpleDirectoryReader(
-    input_files=["data/ASE2.pdf"]
-).load_data()
-ASE_doc2 = SimpleDirectoryReader(
-    input_files=["data/ASE.pdf"]
-).load_data()
+def main():
+    args = parse_args()
+    data_dir = args.data_dir
+    api_key = args.groq_api_key
+    # load data
+    ASE_doc1 = SimpleDirectoryReader(
+        input_files=[f"{data_dir}ASE2.pdf"]
+    ).load_data()
+    ASE_doc2 = SimpleDirectoryReader(
+        input_files=[f"{data_dir}ASE.pdf"]
+    ).load_data()
 
-# llm = Groq(model="Llama3-70b-8192", api_key="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")
-# build index
-from llama_index.embeddings.fastembed import FastEmbedEmbedding
-embed_model = FastEmbedEmbedding(model_name="BAAI/bge-small-en-v1.5")
-Settings.embed_model = embed_model
-Settings.chunk_size = 512
-llm = Groq(model="Llama3-70b-8192", api_key=api_key)# add app_key
+    # llm = Groq(model="Llama3-70b-8192", api_key="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")
+    # build index
+    from llama_index.embeddings.fastembed import FastEmbedEmbedding
+    embed_model = FastEmbedEmbedding(model_name="BAAI/bge-small-en-v1.5")
+    Settings.embed_model = embed_model
+    Settings.chunk_size = 512
+    llm = Groq(model="Llama3-70b-8192", api_key=api_key)# add app_key
 
-Settings.llm = llm
-Settings.chunk_size = 512
-ase1_index = VectorStoreIndex.from_documents(ASE_doc1, show_progress=True)# this uses the embed model internally
-ase2_index = VectorStoreIndex.from_documents(ASE_doc2)
+    Settings.llm = llm
+    Settings.chunk_size = 512
+    ase1_index = VectorStoreIndex.from_documents(ASE_doc1, show_progress=True)# this uses the embed model internally
+    ase2_index = VectorStoreIndex.from_documents(ASE_doc2)
 
-# persist index
-ase1_index.storage_context.persist(persist_dir="storage/ASE_doc1")
-ase2_index.storage_context.persist(persist_dir="storage/ASE_doc2")
+    # persist index
+    os.makedirs("storage", exist_ok=True)
+    ase1_index.storage_context.persist(persist_dir="storage/ASE_doc1")
+    ase2_index.storage_context.persist(persist_dir="storage/ASE_doc2")
 
 
 
 
-ase1_engine = ase1_index.as_query_engine(similarity_top_k=3)
-ase2_engine = ase2_index.as_query_engine(similarity_top_k=3)
+    ase1_engine = ase1_index.as_query_engine(similarity_top_k=3)
+    ase2_engine = ase2_index.as_query_engine(similarity_top_k=3)
 
-query_engine_tools = [
-    QueryEngineTool(
-        query_engine=ase1_engine,
-        metadata=ToolMetadata(
-            name="ASE1",
-            description=(
-                "ASE documentation 1 "
-                "Use a detailed plain text question as input to the tool."
+    query_engine_tools = [
+        QueryEngineTool(
+            query_engine=ase1_engine,
+            metadata=ToolMetadata(
+                name="ASE1",
+                description=(
+                    "ASE documentation 1 "
+                    "Use a detailed plain text question as input to the tool."
+                ),
             ),
         ),
-    ),
-    QueryEngineTool(
-        query_engine=ase2_engine ,
-        metadata=ToolMetadata(
-            name="ASE2",
-            description=(
-                "ASE documentation 2 "
-                "Use a detailed plain text question as input to the tool."
+        QueryEngineTool(
+            query_engine=ase2_engine ,
+            metadata=ToolMetadata(
+                name="ASE2",
+                description=(
+                    "ASE documentation 2 "
+                    "Use a detailed plain text question as input to the tool."
+                ),
             ),
         ),
-    ),
 
-]
+    ]
 
-## setup ReAct Agent
+    ## setup ReAct Agent
 
-# [Optional] Add Context
-context = """\
-You are a Molecular dynamics simulation expert in python.\
-    You will answer questions about writeing python snippets for getting properties of materials\
-    as a material  scientist.\
-"""
-
+    # [Optional] Add Context
+    context = """\
+    You are a Molecular dynamics simulation expert in python.\
+        You will answer questions about writeing python snippets for getting properties of materials\
+        as a material  scientist.\
+    """
 
 
-# llm = OpenAI(model="gpt-3.5-turbo-0613")
+
+    # llm = OpenAI(model="gpt-3.5-turbo-0613")
 
 
-agent = ReActAgent.from_tools(
-    query_engine_tools,
-    llm=llm,
-    verbose=True,
-    context=context
-)
+    agent = ReActAgent.from_tools(
+        query_engine_tools,
+        llm=llm,
+        verbose=True,
+        context=context
+    )
 
-response = agent.chat("give python code for: calculate the bulk modulus of AL using ASE with EMT simulation code")
-print(str(response))
+    response = agent.chat("give python code for: calculate the bulk modulus of AL using ASE with EMT simulation code")
+    print(str(response))
 
-# 
+if __name__ == "__main__":
+    main()
