edaef754a2f076e82ca30520742e0f730981059d
Jan Janßen <janssen@mpie.de>
2024-06-20 22:10:53 +0200

    Fix version numbers

--- setup.py
+++ setup.py
@@ -7,17 +7,16 @@ setup(
     description='Application of Large Language Models (LLM) for computational materials science',
     packages=find_packages(),
     install_requires=[
-        "ase",
-        "langchain",
-        "numexpr",
-        "langchain-openai",
-        "langchain-experimental",
-        "mace",
-        "matplotlib",
-        "langchainhub",
-        "pandas",
-        "lxml",
-        "mendeleev"
+        "ase==3.23.0",
+        "langchain==0.2.5",
+        "numexpr==2.10.0",
+        "langchain-openai==0.1.8",
+        "langchain-experimental==0.0.61",
+        "mace=0.3.5",
+        "matplotlib==3.8.4",
+        "pandas==2.2.2",
+        "lxml==4.9.4",
+        "mendeleev==0.17.0"
     ],
     extra_requires={
         "mace": ["mace"],
