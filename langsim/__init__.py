33a816ab92746773df142c9672de78f03c6c7295
Jan Janssen <janssen@mpie.de>
2024-06-21 16:46:55 +0200

    Some more refactoring

--- langsim/__init__.py
+++ langsim/__init__.py
@@ -1,11 +1,8 @@
-from langsim.shell import dialog as dialog_python
-
-
 def load_ipython_extension(ipython):
     """
     Any module file that define a function named `load_ipython_extension`
     can be loaded via `%load_ext module.path` or be configured to be
     autoloaded by IPython at startup time.
     """
-    from .magics import CompMatMagics
+    from langsim.magics import CompMatMagics
     ipython.register_magics(CompMatMagics)
--- langsim/shell.py
+++
@@ -1,9 +0,0 @@
-from langsim.llm import get_executor
-import matplotlib.pyplot as plt
-
-
-def dialog(OPENAI_API_KEY):
-    agent_executor = get_executor(OPENAI_API_KEY=OPENAI_API_KEY)
-    while True:
-        lst = list(agent_executor.stream({"conversation": [("user", input())]}))
-        plt.show()
