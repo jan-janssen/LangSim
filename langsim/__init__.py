85184183db5358ca12cf263eb2c073b753ede14c
jan-janssen <jan-janssen@users.noreply.github.com>
2024-09-13 09:49:00 +0000

    Deploying to gh-pages from  @ 8b961ccfba0bd8c30f48acfcf33c5fe022fda63e 🚀

--- langsim/__init__.py
+++
@@ -1,9 +0,0 @@
-def load_ipython_extension(ipython):
-    """
-    Any module file that define a function named `load_ipython_extension`
-    can be loaded via `%load_ext module.path` or be configured to be
-    autoloaded by IPython at startup time.
-    """
-    from langsim.magics import CompMatMagics
-
-    ipython.register_magics(CompMatMagics)
