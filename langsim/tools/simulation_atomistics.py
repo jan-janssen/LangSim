354153eede011b4e5717e01e0fd0638a6c9b9a20
Jan Janssen <jan-janssen@users.noreply.github.com>
2025-07-14 11:03:13 +0200

    Update simulation_atomistics.py

--- langsim/tools/simulation_atomistics.py
+++ langsim/tools/simulation_atomistics.py
@@ -53,18 +53,14 @@ def get_atom_dict_equilibrated_structure(
     Returns:
         AtomsDict: DataClass representing the equilibrated atomic structure
     """
-    try:
-        task_dict = optimize_positions_and_volume(structure=Atoms(**atom_dict.dict()))
-        atoms = evaluate_with_ase(
-            task_dict=task_dict,
-            ase_calculator=get_calculator(calculator_str=calculator_str),
-            ase_optimizer=LBFGS,
-            ase_optimizer_kwargs={"fmax": 0.000001},
-        )["structure_with_optimized_positions_and_volume"]
-        return AtomsDict(**{k: v.tolist() for k, v in atoms.todict().items()})
-    except Exception as error:
-        # handle the exception
-        return "An exception occurred: {}"
+    task_dict = optimize_positions_and_volume(structure=Atoms(**atom_dict.dict()))
+    atoms = evaluate_with_ase(
+        task_dict=task_dict,
+        ase_calculator=get_calculator(calculator_str=calculator_str),
+        ase_optimizer=LBFGS,
+        ase_optimizer_kwargs={"fmax": 0.000001},
+    )["structure_with_optimized_positions_and_volume"]
+    return AtomsDict(**{k: v.tolist() for k, v in atoms.todict().items()})
 
 
 @tool
