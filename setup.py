57a64493bf4bb94dd853902134de2318dd0f617d
Jan Janssen <jan-janssen@users.noreply.github.com>
2025-07-14 11:42:40 +0200

    Update environment (#87)
    * Update environment
    
    * Update environment.yml
    
    * Update unittests.yml
    
    * Update unittests.yml
    
    * Update unittests.yml
    
    * Update environment.yml
    
    * Update setup.py

--- setup.py
+++ setup.py
@@ -7,25 +7,25 @@ setup(
     description='Application of Large Language Models (LLM) for computational materials science',
     packages=find_packages(),
     install_requires=[
-        "ase==3.23.0",
-        "atomistics==0.1.32",
-        "langchain==0.2.5",
-        "numexpr==2.10.0",
-        "langchain-openai==0.1.8",
-        "matplotlib==3.8.4",
-        "pandas==2.2.2",
-        "lxml==4.9.4",
-        "mendeleev==0.17.0",
+        "ase==3.25.0",
+        "atomistics==0.2.5",
+        "langchain==0.3.26",
+        "numexpr==2.10.2",
+        "langchain-openai==0.3.27",
+        "matplotlib==3.10.3",
+        "pandas==2.3.1",
+        "lxml==5.4.0",
+        "mendeleev==0.19.0",
     ],
     extras_require={
         "mace": ["mace==0.3.5"],
         "alloy": [
             "sqsgenerator==0.3",
-            "structuretoolkit==0.0.28",
+            "structuretoolkit==0.0.32",
         ],
         "ipython": [
-            "ipython==6.2.0",
-            "langchain-experimental==0.0.61",
+            "ipython==8.25.0",
+            "langchain-experimental==0.3.4",
         ],
     }
 )
